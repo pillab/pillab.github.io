413821f6354f29b8a8de6ca4a432952b4caf5f6f
Xiangming Meng <mengxm11@gmail.com>
2025-02-21 13:05:50 +0800

    Update publications.js

--- assets/js/publications.js
+++ assets/js/publications.js
@@ -1164,87 +1164,6 @@
             }
         }
 
-
-// const MXM = "Xiangming Meng",
-//       YK = "Yoshiyuki Kabashima",
-
-// // ===========================================================
-// //   这里添加文章starting
-// // ==========================================================
-//       , ye = [{
-//         name: "Publications",
-//         publications: [
-//             {
-//                 authors: [MXM, YK],
-//                 authorsText: "Xiangming Meng*, Yoshiyuki Kabashima",
-//                 image: "",
-//                 title: "Diffusion Model Based Posterior Sampling for Noisy Linear Inverse Problems",
-//                 info: "ACML 2024 - Best Paper Runner-Up Award",
-//                 refs: [
-//                     { href: "https://arxiv.org/abs/2211.12343", text: "arXiv" },
-//                     { href: "https://github.com/mengxiangming/dmps", text: "code" }
-//                 ]
-//             },
-//             {
-//                 authors: [MXM, YK],
-//                 authorsText: "Xiangming Meng*, Yoshiyuki Kabashima",
-//                 image: "",
-//                 title: "QCM-SGM+: Improved Quantized Compressed Sensing With Score-Based Generative Models",
-//                 info: "AAAI 2024",
-//                 refs: [
-//                     { href: "https://arxiv.org/abs/2302.00919v2", text: "arXiv" },
-//                     { href: "https://github.com/mengxiangming/QCS-SGM-plus", text: "code" }
-//                 ]
-//             },
-//             {
-//                 authors: [MXM],
-//                 authorsText: "Xiangming Meng*, Roman Bachmann, Mohammad Emtiyaz Khan",
-//                 image: "",
-//                 title: "Training Binary Neural Networks using the Bayesian Learning Rule",
-//                 info: "ICML 2020",
-//                 refs: [
-//                     { href: "https://arxiv.org/abs/2002.10778", text: "arXiv" },
-//                     { href: "https://github.com/team-approx-bayes/BayesBiNN", text: "code" }
-//                 ]
-//             },
-//             {
-//                 authors: [MXM],
-//                 authorsText: "Xiangming Meng*, Lei Zhang, Chao Wang, Wenjin Wang, Yiqun Wu, Yan Chen",
-//                 image: "",
-//                 title: "Advanced NOMA Receivers From a Unified Variational Inference Perspective",
-//                 info: "JSAC 2021",
-//                 refs: [
-//                     { href: "https://ieeexplore.ieee.org/abstract/document/9181630", text: "IEEE" }
-//                 ]
-//             },
-//             {
-//                 authors: [MXM],
-//                 authorsText: "Xiangming Meng*, Sheng Wu, Linling Kuang, Jianhua Lu",
-//                 image: "",
-//                 title: "An Expectation Propagation Perspective on Approximate Message Passing",
-//                 info: "SPL 2015",
-//                 refs: [
-//                     { href: "https://ieeexplore.ieee.org/abstract/document/7008468", text: "IEEE" }
-//                 ]
-//             },
-//             {
-//                 authors: [MXM],
-//                 authorsText: "Xiangming Meng*, Sheng Wu, Jiang Zhu",
-//                 image: "",
-//                 title: "A unified Bayesian Inference Framework for Generalized Linear Models",
-//                 info: "SPL 2018",
-//                 refs: [
-//                     { href: "https://arxiv.org/abs/1712.10288", text: "arXiv" },
-//                     { href: "https://ieeexplore.ieee.org/abstract/document/8244269/", text: "IEEE" }
-//                 ]
-//             }
-//         ]
-//     }];
-// // ===========================================================
-// //   这里添加文章ending
-// // ==========================================================
-    
-
     
         const Se = "Juan Carlos Niebles"
           , Pe = "L. Fei-Fei"
@@ -1260,14 +1179,7 @@
 // ==========================================================
           , ye = [{
             name: "Abstracts",
-            publications: [{
-                authors: [De],
-                authorsText: "J. Clemons, Y. Bao, M. Bagra, T. Austin, S. Savarese,",
-                image: "",
-                title: "Scene Understanding for the Visually Impaired Using Visual Sonification by Visual Feature Analysis and Auditory Signature",
-                info: "Proc. of Vision Science Society, May 2012.",
-                refs: []
-            }, 
+            publications: [
             {
                 authors: [MXM, YK],
                 authorsText: "Xiangming Meng*, Yoshiyuki Kabashima,",
